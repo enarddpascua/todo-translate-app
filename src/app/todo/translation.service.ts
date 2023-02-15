dbc384e11981d105664479e7da3801a77a0df7e4
Enard <enard@Enards-MacBook-Pro.local>
2023-02-15 14:57:09 +1100

    remove api key

--- src/app/todo/translation.service.ts
+++ src/app/todo/translation.service.ts
@@ -8,7 +8,7 @@ import { TranslationRequestBody,TranslationResponseBody } from "./translation.mo
 })
 export class TranslationService{
     url = 'https://translation.googleapis.com/language/translate/v2?';
-    apiKey = 'key=AIzaSyDYPFynlqZXS-83gsSe8Wu9H7NY8p2KmoI';
+    apiKey = '';
 
     constructor(private http: HttpClient){}
 
