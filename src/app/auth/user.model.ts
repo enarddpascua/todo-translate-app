ce16a4653b430a6cfef020be7013df770bea72fc
Enard <enard@Enards-MacBook-Pro.local>
2023-02-15 14:47:46 +1100

    add user model

---
+++ src/app/auth/user.model.ts
@@ -0,0 +1,14 @@
+
+export interface UserWithId {
+    uid:number,
+    email:string,
+    password: string,
+    role:string
+}
+
+
+export interface User {
+    email:string,
+    password: string,
+    role:string
+}
