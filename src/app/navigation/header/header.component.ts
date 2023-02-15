53d99a3a748bbe87e25d4cd990bafe8ee2ffdd8c
Enard <enard@Enards-MacBook-Pro.local>
2023-02-15 14:48:41 +1100

    add dynamic render of nav buttons and functionalities

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { AuthenticationService } from 'src/app/auth/authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -6,9 +9,27 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+loggedIn:any = sessionStorage.getItem('x');
+
+  
 @Output() sideNavToggle = new EventEmitter<void>();
+
+constructor(private authenticationService:AuthenticationService, private snackBar: MatSnackBar){}
+
+public get isLoggedIn(){
+  return this.authenticationService.isLoggedIn();
+}
 
  onToggleSideNav(){
   this.sideNavToggle.emit();
+ }
+
+ onLogout(){
+  console.log(this.isLoggedIn);
+  this.snackBar.open("Successfully logged out", "Ok",{
+    duration: 3000,
+    verticalPosition:'top'
+  });
+  this.authenticationService.logout();
  }
 }
