7591aa22a82076b66b212d4c408ffa028f1e7ee6
Enard <enard@Enards-MacBook-Pro.local>
2023-02-15 14:48:13 +1100

    add signup template and register function

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-signup',
@@ -8,7 +11,22 @@ import { NgForm } from '@angular/forms';
 })
 export class SignupComponent {
 
+  constructor(private authenticationService:AuthenticationService, private router: Router, private snackBar:MatSnackBar){}
+
   onSubmit(form:NgForm){
-    console.log(form);
+    const reqBody={
+      email:form.value.email,
+      password:form.value.password,
+      role:"user"
+    }
+
+    this.authenticationService.signup(reqBody).subscribe(result =>{
+      if(result==="email already exists."){
+        this.snackBar.open(result,'Ok');
+        return;
+      }
+      form.reset();
+      this.router.navigate(['/login']);
+    });
   }
 }
